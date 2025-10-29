1e268a78c8e6e08b4b779ad270a7c476acc2ffa9
Andrés Felipe Salamanca Silva <56135223+MRPinas2100@users.noreply.github.com>
2025-10-28 19:35:25 -0500

    Add Files

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="min-h-dvh w-full p-4 bg-gray-50 text-gray-900 flex flex-col">
+    <MainHeader />
+    <div class="flex-1">
+      <!-- aquí irá el contenido -->
+    </div>
+    <MainFooter />
+  </main>
+</template>
+
+<script setup>
+import MainHeader from './components/MainHeader.vue';
+import MainFooter from './components/MainFooter.vue';
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/MainHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="border-b bg-white/90 backdrop-blur">
+    <div
+      class="mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg p-4"
+    >
+      <div class="flex items-center justify-between gap-3">
+        <h1 class="text-xl font-semibold md:text-2xl">
+          Project English Teacher
+        </h1>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
